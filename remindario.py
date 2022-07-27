9c76065aaf5df9ef70ac9a194a81b778de9636e2
Sara Jeiter-Johnson <sjeiterj@u.rochester.edu>
2022-07-27 16:07:03 -0400

    error handling and quick command added

--- remindario.py
+++ remindario.py
@@ -23,11 +23,19 @@ async def on_ready():
     print("Starting...")
     scheduler.start()
 
-#helper
+#helpers
 def str_to_datetime(str):
     #Ex str: '12:34 PM 5/6/22'
     dt = datetime.strptime(str, '%I:%M %p %m/%d/%y')
     return dt
+
+def dt_is_incorrect_format(message):
+        try:
+            str_to_datetime(message)
+        except:
+            print('Time is Incorrect Format')
+            return True
+        return False
 
 #adds reminder to scheduler
 def create_reminder(what, who, when):
@@ -41,27 +49,63 @@ async def send_reminder(reminder):
     channel = discord.utils.get(bot.get_all_channels(), name=CHANNEL)
     await channel.send(reminder)
 
-@bot.command(name='test')
+#commands
+@bot.command()
 async def test(ctx):
     scheduler.add_job(send_reminder, args=['This is a test.'])
+
+@bot.command(name='set-reminder-quick')
+async def set_reminder(ctx, who, what, when):
+
+    requester = ctx.message.author
+
+    def check_author(message):
+        return message.author == requester
+
+    if (who == None or what == None or when == None):
+        await ctx.send('Sorry, not enough arguments. Try \'set-reminder\' instead.')
+    elif dt_is_incorrect_format(when):
+        await ctx.send('Hmm, something is not formatted right. Try \'set-reminder\' instead.')
+    else:
+        m = 'Let\'s make sure I have this right. You would like to send a message to ' + who + ' about ' + '\"' + what + '\"' + ' at ' + when + '? y/n'
+        await ctx.send(m)
+        response = await bot.wait_for('message', check=check_author)
+        if response.content == 'y':
+                create_reminder(what, who, when)
+                await ctx.send('Alrighty! Reminder added.')
+        else:
+            await ctx.send('Reminder canceled.')
+
 
 @bot.command(name='set-reminder')
 async def set_reminder(ctx):
 
-    def check(message):
-        return message.content != 'quit'
+    requester = ctx.message.author
+
+    def check_author(message):
+        return message.author == requester
 
     await ctx.send("Who am I reminding?")
-    who = await bot.wait_for('message', check=check)
+    who = await bot.wait_for('message', check=check_author)
 
     await ctx.send("What am I reminding them about?")
-    what = await bot.wait_for('message', check=check)
+    what = await bot.wait_for('message', check=check_author)
 
     await ctx.send("When should I remind them? (Please respond in the format of this example: 12:34 PM 5/6/22)")  
-    when = await bot.wait_for('message', check=check)
+    when = await bot.wait_for('message', check=check_author)
 
-    create_reminder(what.content, who.content, when.content)
-    await ctx.send('Reminder added.')
+    while dt_is_incorrect_format(when.content):
+        await ctx.send('Sorry, the time is in an incorrect format. Please try again.')
+        when = await bot.wait_for('message', check=check_author)
+
+    m = 'Let\'s make sure I have this right. You would like to send a message to ' + who.content + ' about ' + '\"' + what.content + '\"' + ' at ' + when.content + '? y/n'
+    await ctx.send(m)
+    response = await bot.wait_for('message', check=check_author)
+    if response.content == 'y':
+            create_reminder(what.content, who.content, when.content)
+            await ctx.send('Alrighty! Reminder added.')
+    else:
+        await ctx.send('Reminder canceled.')
    
 
 
